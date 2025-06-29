f4781717e7548da9f8370d45b94b7291a4e25656
Shiva Devarajan <arcubedgs@gmail.com>
2025-06-29 14:35:34 -0400

    Clean up generate replays

--- generate_replays.py
+++ generate_replays.py
@@ -5,13 +5,15 @@ import re
 from collections import defaultdict
 
 replay_embed_location="https://staraptorshowdown.com/js/replay-embed.js"
+# Helpful to avoid regenerating replays to speed up script
 old_months = ["2023-01", "2023-02", "2023-03", "2023-04",
               "2023-05", "2023-06", "2023-07", "2023-08",
               "2023-09", "2023-10", "2023-11", "2023-12",
               "2024-01", "2024-02", "2024-03", "2024-04",
               "2024-05", "2024-06", "2024-07", "2024-08",
               "2024-09", "2024-10", "2024-11", "2024-12", "2025-01",
-              "2025-02", "2025-03", "2025-04"]
+              "2025-02", "2025-03", "2025-04", "2025-05"]
+# old_months = []
 old_months = [f"../pokemon-showdown/logs/{m}" for m in old_months]
 subfolders = [f.path for f in os.scandir(
     "../pokemon-showdown/logs/") if f.is_dir() and f.path[-3] == "-"]
@@ -36,8 +38,7 @@ for format, log_jsons in log_json_dict.items():
     if not os.path.exists(format_dir):
         os.makedirs(format_dir)
     for log_json in log_jsons:
-        if log_json.endswith(".DS_Store"):
-            os.remove(log_json)
+        if not log_json.endswith(".json"):
             continue
         with open(log_json) as file:
             log = json.load(file)
@@ -51,6 +52,10 @@ for format, log_jsons in log_json_dict.items():
         name = f"{id}_{p1}_vs_{p2}.html"
         path = f"{format_dir}/{name}"
         logPath = f"{path}.log"
+        # Skip if the replay already exists
+        if (os.path.exists(path) and os.path.exists(logPath)):
+            continue
+
         replay_object = Replay.create_replay_object(log, show_full_damage=True)
         html = Download.create_replay(replay_object,
                                       replay_embed_location=replay_embed_location)

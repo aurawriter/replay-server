51e8122621c0f50d09674f425cc987f52c33a65e
aurawriter <30663874+aurawriter@users.noreply.github.com>
2025-08-08 11:46:45 -0700

    Update generate_csv.py

--- generate_csv.py
+++ generate_csv.py
@@ -85,7 +85,7 @@ def process_logs(folder_path, output_csv):
                             tier = root.split("/")[-2]
                             p1id = re.sub(r'\W+', '', p1).replace("_", "")
                             p2id = re.sub(r'\W+', '', p2).replace("_", "")
-                            link = f"http://51.81.209.222/replays/{tier}/{room_id}_{p1id}_vs_{p2id}.html"
+                            link = f"https://aurorashowdown.com/replays/{tier}/{room_id}_{p1id}_vs_{p2id}.html"
 
                             # Write the values to the CSV file
                             csv_writer.writerow({
